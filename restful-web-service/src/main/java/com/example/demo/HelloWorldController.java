b1945378a101f869bda45e1d2591d65249f6abf3
DESKTOP-VSMAJKH\rbfla <rbfla0426_1@naver.com>
2022-11-11 19:24:48 +0900

    사용자 전체 조회, 사용자 조회, 사용자 생성

--- restful-web-service/src/main/java/com/example/demo/HelloWorldController.java
+++ restful-web-service/src/main/java/com/example/demo/HelloWorldController.java
@@ -13,14 +13,14 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World";
 	}
-	
+
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
 	}
-	
-	@GetMapping(path = "/hello-world-bean")
+
+	@GetMapping(path = "/hello-world-bean/{name}")
 	public HelloWorldBean helloWorldBean(@PathVariable String name) {
-		return new HelloWorldBean(String.format("Hello World, %s",name));
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
