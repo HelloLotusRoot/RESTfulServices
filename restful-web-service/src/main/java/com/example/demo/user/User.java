e2060fc26dd8e974f959465146b2e6fbfd6bac3e
DESKTOP-VSMAJKH\rbfla <rbfla0426_1@naver.com>
2022-11-12 20:27:06 +0900

    Validation 추가

--- restful-web-service/src/main/java/com/example/demo/user/User.java
+++ restful-web-service/src/main/java/com/example/demo/user/User.java
@@ -1,5 +1,8 @@
 package com.example.demo.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 import java.util.Date;
 
 import lombok.AllArgsConstructor;
@@ -9,6 +12,9 @@ import lombok.Data;
 @AllArgsConstructor
 public class User {
 	private Integer id;
+
+	@Size(min = 2)
 	private String name;
+	@Past
 	private Date joinDate;
 }
