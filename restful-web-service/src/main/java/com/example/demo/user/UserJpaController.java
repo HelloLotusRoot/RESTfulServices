39d472fded26d354e7c83cff316a2adcb47c8f70
DESKTOP-VSMAJKH\rbfla <rbfla0426_1@naver.com>
2022-11-15 00:29:31 +0900

    JPA GET HTTP Method

--- restful-web-service/src/main/java/com/example/demo/user/UserJpaController.java
+++ restful-web-service/src/main/java/com/example/demo/user/UserJpaController.java
@@ -1,10 +1,16 @@
 package com.example.demo.user;
 
-
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +22,22 @@ public class UserJpaController {
 
 	// http://localhost:8088/jpa/users
 	@GetMapping("/users")
-    public List<User> retrieveAllUsers() {
-        return userRepository.findAll();
-    }
+	public List<User> retrieveAllUsers() {
+		return userRepository.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public EntityModel<User> retrieveAllUsers(@PathVariable int id) {
+		Optional<User> user = userRepository.findById(id);
+
+		if (!user.isPresent()) {
+			throw new UserNotFoundException(String.format("ID[%s] not found", id));
+		}
+
+		EntityModel<User> userModel = EntityModel.of(user.get());
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		userModel.add(linkTo.withRel("all-users"));
+
+		return userModel;
+	}
 }
--- restful-web-service/src/main/resources/data.sql
+++ restful-web-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into users values(90001, NOW(), 'User1', 'test1111', '701010-1111111');
-insert into users values(90002, NOW(), 'User2', 'test2222', '801010-2222222');
-insert into users values(90003, NOW(), 'User3', 'test3333', '901010-1111111');
+insert into users values(1, NOW(), 'User1', 'test1111', '701010-1111111');
+insert into users values(2, NOW(), 'User2', 'test2222', '801010-2222222');
+insert into users values(3, NOW(), 'User3', 'test3333', '901010-1111111');
